79ce550f4d4bae105b2aba039cd91313f74683b5
minthwayhtut <minthwayhtut568@gmail.com>
2026-02-17 10:20:22 +0100

    Setting DEBUG to True

--- minimart_backend/minimart_backend/settings.py
+++ minimart_backend/minimart_backend/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-3l#^s_*3&sqe&x07cl-t5q!6f*dbs5ya04mm^pquh1w^!b!r8l'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
